af3ce2e71e6362c94928aa8b293c77e64d381590
Raghu <skraghunandan11@gmail>
2018-04-05 10:32:27 +0400

    S3 complete

--- aws/S3_bucket.py
+++ aws/S3_bucket.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 import boto3
 from boto3.session import Session
-s3 = boto3.setup_default_session(region_name='us-west-2')
-session = Session(aws_access_key_id='AKIAJUJE2U4ES67MISGQ',aws_secret_access_key='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')
+session = Session(aws_access_key_id='xvz',aws_secret_access_key='xyz',region_name='us-east-1')
 
 s3 = session.resource('s3')
-s3.create_bucket(Bucket='myfbucket789076541253334')
+s3.create_bucket(Bucket='everythingidos3')
